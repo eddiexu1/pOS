8abd1e849ba8b7958735a2d1cf35cc339a090bfd
Eddie Xu <eddiexu@cs.utexas.edu>
2021-11-14 19:07:14 -0600

    added spamon

--- kernel/sys.cc
+++ kernel/sys.cc
@@ -5,7 +5,18 @@
 #include "machine.h"
 
 extern "C" int sysHandler(uint32_t eax, uint32_t *frame) {
-    MISSING();
+    if (eax == 0) {
+        Debug::shutdown();
+    }
+    if (eax == 1) {
+        uint32_t* usr_stack = (uint32_t*) frame[3];
+        size_t len = (size_t) usr_stack[3];
+        char* buf = (char*) usr_stack[2];
+        for (uint32_t i = 0; i < len; i++) {
+            Debug::printf("%c", buf[i]);
+        }
+        return len;
+    }
     return 0;
 }
 
